b67852a4ad38467385d2a757349d783c5b30edc8
Trí Tạ <tribietyeu2006@gmail.com>
2026-03-13 14:03:08 +0700

    Add function to print perfect numbers less than n

---
+++ bai23.py
@@ -0,0 +1,12 @@
+n = int(input("Nhap n: "))
+
+print("Cac so hoan hao nho hon", n, "la:", end=" ")
+
+for i in range(1, n):
+    tong = 0
+    for j in range(1, i):
+        if i % j == 0:
+            tong += j
+    
+    if tong == i:
+        print(i, end=" ")
